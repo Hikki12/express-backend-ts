0a88a487e896d62c78e7ce3b52e0255a2a6e6de0
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-08-04 20:20:35 -0500

    initial commit

---
+++ src/routes/products.router.ts
@@ -0,0 +1,7 @@
+import express, { Express, Request, Response } from 'express'
+const router = express.Router()
+
+router.get('/', (req: Request, res: Response) => {
+  console.log(req)
+  res.end()
+})
