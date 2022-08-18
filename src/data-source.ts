10117f18867f803703936fdf1ca2282b7f7436df
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-08-18 10:44:33 -0500

    synchronize enabled

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,6 +1,8 @@
 import { DataSource } from "typeorm"
 import  { config } from "./config";
 import { Product } from "./entity/product.entity";
+import { User } from "./entity/user.entity";
+import { Customer } from "./entity/customer.entity"
 
 
 export const AppDataSource = new DataSource({
@@ -10,10 +12,12 @@ export const AppDataSource = new DataSource({
     username: config.DB_USER,
     password: config.DB_PASSWORD,
     database: config.DB_NAME,
-    synchronize: false,
+    synchronize: true,
     logging: true,
     entities: [
-      Product
+      Product,
+      User,
+      Customer
     ],
     subscribers: [],
     migrations: []
